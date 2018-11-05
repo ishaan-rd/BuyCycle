76c1159170796d758e5865a8221f8e48c36666c1
ishaan-rd <ishaanrd6@gmail.com>
2018-11-05 10:55:26 +0530

    fine almost done

--- routes/fine.js
+++ routes/fine.js
@@ -17,10 +17,25 @@ router.route('/')
 
             res.render('fine', { results: results, admin: 'true' })
         })
-    } else {}
+    } else {
+        res.render('fine', { owner: 'true' })
+    }
 })
 .post((req, res, next) => {
-    
+    db.query('select * from rent r, users u where u.username = r.re_own_roll and u.id = ? order by rent_id desc limit 0,1', [req.user.user_id],
+    (error, results, fields) => {
+        if (error) throw error
+        // console.log(req.body)
+        // console.log(results)
+        var amount = 0
+
+        db.query('insert into fine (crossed_time, damage, amount, fi_own_roll, fi_ren_roll) values (?, ?, ?, ?, ?)', [req.body.time, req.body.damage, amount, results[0].re_own_roll, results[0].re_ren_roll],
+        (error, results, fields) => {
+            if (error) throw error
+
+            res.redirect('/')
+        })
+    })
 })
 
 module.exports = router
